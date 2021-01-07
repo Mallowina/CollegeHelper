141d33cf5c0c384e4e32263120ddbf35a9690ce9
Mallowina <2003ksenia@mail.ru>
2021-01-07 17:01:16 +0300

    add changes

---
+++ app/src/main/java/com/example/collegehelper/ui/schedule/ScheduleFragment.java
@@ -0,0 +1,35 @@
+package com.example.collegehelper.ui.schedule;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.collegehelper.R;
+
+public class ScheduleFragment extends Fragment {
+
+    private ScheduleViewModel scheduleViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        scheduleViewModel =
+                new ViewModelProvider(this).get(ScheduleViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_schedule, container, false);
+        final TextView textView = root.findViewById(R.id.text_schedule);
+        scheduleViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
