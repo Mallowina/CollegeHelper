62725564b94a65671ef712f22329f323c7e94217
kaakaasha <74490635+kaakaasha@users.noreply.github.com>
2021-01-08 14:30:16 +0300

    попытка переходов

---
+++ app/src/main/java/com/example/collegehelper/Authorisation.java
@@ -0,0 +1,31 @@
+package com.example.collegehelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Authorisation extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.autoriz);
+
+    }
+    // Метод обработки нажатия на кнопку
+    public void openmainactivity (View view) {
+        // действия, совершаемые после нажатия на кнопку
+        // Создаем объект Intent для вызова новой Activity
+        Intent intent = new Intent(Authorisation.this, MainActivity.class);
+        // запуск activity
+        startActivity(intent);
+    }
+    public void openreg (View view) {
+        // действия, совершаемые после нажатия на кнопку
+        // Создаем объект Intent для вызова новой Activity
+        Intent intent = new Intent(Authorisation.this, Registration.class);
+        // запуск activity
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/collegehelper/Registration.java
@@ -0,0 +1,16 @@
+package com.example.collegehelper;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+
+    }
+}
